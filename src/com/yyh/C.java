976bdea25bee734e431c41bd84644f38f5e388ae
goddog <yangyihang5@163.com>
2022-03-25 21:33:03 +0800

    输入多组数据, 输出每组a+b, 遇到输入0,0停止输出

---
+++ src/com/yyh/C.java
@@ -0,0 +1,15 @@
+package com.yyh;
+import java.util.Scanner;
+public class C {
+    public static void main(String[] argv){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+        }
+    }
+}
