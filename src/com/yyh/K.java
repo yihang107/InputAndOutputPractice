1a91697b534fce97774b35783d301d88f220c915
goddog <yangyihang5@163.com>
2022-03-25 22:17:45 +0800

    输入多组测试用例，每组两个数, 输出每组的和

---
+++ src/com/yyh/K.java
@@ -0,0 +1,10 @@
+package com.yyh;
+import java.util.Scanner;
+public class K {
+    public static void main(String[] argv) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            System.out.println(sc.nextInt() + sc.nextInt());
+        }
+    }
+}
